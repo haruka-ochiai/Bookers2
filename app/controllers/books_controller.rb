4384a28a89471a5c9c0e0766501bfd9dd6760583
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-07-12 12:14:46 +0000

    Showページ、Indexページ雛形

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,24 +4,39 @@ class BooksController < ApplicationController
   end
   
   def create
+    # @book = Book.new(book_params)
+    # @book.user_id = current_user.id
+    # @book.save
+    # redirect_to book_path(@book.id)
+    
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to user_path(currnet_user.id)
+    redirect_to book_path(@book.id)
   end
 
   def index
-    @user = User.find(params[:id])
-    
-    @books = @user.books
+    @books = Book.all
+    @user = current_user
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @books = @user.books
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+ 
   private
   
-  def books
+  def book_params
+    # params.require(:book).permit(:title, :body)
+    
     params.require(:book).permit(:title, :body)
   end
 end
